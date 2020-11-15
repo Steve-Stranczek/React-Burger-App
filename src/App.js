30cf81908cd14677c24e18ab8d4a7c1d39a509fa
Steve Stranczek <stevestranczek@gmail.com>
2020-11-15 14:58:02 -0700

    Added burger builder class

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import React, { Component } from "react";
 import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <p>Test</p>
+          <BurgerBuilder />
         </Layout>
       </div>
     );
