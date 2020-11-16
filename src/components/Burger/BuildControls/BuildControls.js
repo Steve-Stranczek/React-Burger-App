6d6875869080c6fe7398d15231e7e85777740a74
Steve Stranczek <stevestranczek@gmail.com>
2020-11-16 12:41:54 -0700

    Added build controls skeleton

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "./buildControls/buildControls.module.css";
+
+const buildControls = (props) => (
+  <div>
+    <p>my p</p>
+  </div>
+);
+
+export default buildControls;
