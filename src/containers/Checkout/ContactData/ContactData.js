49a402455f65b9417861928f0e1569ef6a867eef
Steve Stranczek <stevestranczek@gmail.com>
2020-11-23 12:53:56 -0700

    Added a custom input type

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from "../../../components/UI/Button/Button";
 import classes from "./ContactData.module.css";
 import axios from "../../../Axios/axios-orders";
 import Spinner from "../../../components/UI/Spinner/Spinner";
+import Input from "../../../components/UI/Forms/Input/Input";
 
 class ContactData extends Component {
   state = {
@@ -48,26 +49,21 @@ class ContactData extends Component {
   render() {
     let form = (
       <form>
-        <input
-          className={classes.Input}
-          type="text"
-          name="name"
-          placeholder="Name"
-        />
-        <input
-          className={classes.Input}
+        <Input inputtype="input" type="text" name="name" placeholder="Name" />
+        <Input
+          inputtype="input"
           type="email"
           name="email"
           placeholder="Email"
         />
-        <input
-          className={classes.Input}
+        <Input
+          inputtype="input"
           type="text"
           name="street"
           placeholder="Street"
         />
-        <input
-          className={classes.Input}
+        <Input
+          inputtype="input"
           type="text"
           name="postal"
           placeholder="PostalCode"
