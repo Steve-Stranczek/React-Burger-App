df004f70fcafc524f15bf941d7fdf7b5f998ab74
Steve Stranczek <stevestranczek@gmail.com>
2020-11-23 14:55:18 -0700

    Added visual feedback to form validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -109,7 +109,13 @@ class ContactData extends Component {
       });
   };
 
-  checkValidity = () => {};
+  checkValidity(value, rules) {
+    let isValid = false;
+    if (rules.required) {
+      isValid = value.trim() !== "";
+    }
+    return isValid;
+  }
 
   inputChangedHandler = (event, inputIdentifier) => {
     const updatedForm = {
@@ -119,6 +125,10 @@ class ContactData extends Component {
     const updatedFormElement = { ...updatedForm[inputIdentifier] };
 
     updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(
+      updatedFormElement.value,
+      updatedFormElement.validation
+    );
     updatedForm[inputIdentifier] = updatedFormElement;
     this.setState({ orderForm: updatedForm });
   };
@@ -139,6 +149,8 @@ class ContactData extends Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
+            invalid={!formElement.config.valid}
+            shouldValidate={formElement.config.validation}
             changed={(event) => this.inputChangedHandler(event, formElement.id)}
           />
         ))}
