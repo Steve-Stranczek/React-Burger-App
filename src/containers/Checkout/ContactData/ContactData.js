271d259b014f51383d0b68b20f4166bb2cbaf7db
Steve Stranczek <stevestranczek@gmail.com>
2020-11-23 14:03:42 -0700

    Added two way binding with the input on form so i can use onchange for the form elements

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -82,6 +82,18 @@ class ContactData extends Component {
       });
   };
 
+  inputChangedHandler = (event, inputIdentifier) => {
+    const updatedForm = {
+      ...this.state.orderForm,
+    };
+
+    const updatedFormElement = { ...updatedForm[inputIdentifier] };
+
+    updatedFormElement.value = event.target.value;
+    updatedForm[inputIdentifier] = updatedFormElement;
+    this.setState({ orderForm: updatedForm });
+  };
+
   render() {
     const formElementsArray = [];
     for (let key in this.state.orderForm) {
@@ -98,6 +110,7 @@ class ContactData extends Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
+            changed={(event) => this.inputChangedHandler(formElement.id)}
           />
         ))}
         <Button btnType="Success" clicked={this.orderHandler}>
