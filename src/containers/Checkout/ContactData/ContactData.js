15d508dc4e6ba5041d004d7820019a3ecbf2e2eb
Steve Stranczek <stevestranczek@gmail.com>
2020-11-24 19:41:29 -0700

    finished up form validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -19,6 +19,7 @@ class ContactData extends Component {
           required: true,
         },
         valid: false,
+        touched: false,
       },
       street: {
         elementType: "input",
@@ -31,6 +32,7 @@ class ContactData extends Component {
           required: true,
         },
         valid: false,
+        touched: false,
       },
       zipCode: {
         elementType: "input",
@@ -43,6 +45,7 @@ class ContactData extends Component {
           required: true,
         },
         valid: false,
+        touched: false,
       },
       country: {
         elementType: "input",
@@ -55,6 +58,7 @@ class ContactData extends Component {
           required: true,
         },
         valid: false,
+        touched: false,
       },
       email: {
         elementType: "input",
@@ -67,6 +71,7 @@ class ContactData extends Component {
           required: true,
         },
         valid: false,
+        touched: false,
       },
       deliveryMethod: {
         elementType: "select",
@@ -77,9 +82,11 @@ class ContactData extends Component {
           ],
         },
         value: "",
+        valid: true,
       },
     },
     loading: false,
+    formIsValid: false,
   };
 
   orderHandler = (event) => {
@@ -129,8 +136,14 @@ class ContactData extends Component {
       updatedFormElement.value,
       updatedFormElement.validation
     );
+    updatedFormElement.touched = true;
     updatedForm[inputIdentifier] = updatedFormElement;
-    this.setState({ orderForm: updatedForm });
+
+    let formIsValid = false;
+    for (let identifier in updatedForm) {
+      formIsValid = updatedForm[identifier].valid && formIsValid;
+    }
+    this.setState({ orderForm: updatedForm, formIsValid: formIsValid });
   };
 
   render() {
@@ -151,10 +164,13 @@ class ContactData extends Component {
             value={formElement.config.value}
             invalid={!formElement.config.valid}
             shouldValidate={formElement.config.validation}
+            touched={formElement.config.touched}
             changed={(event) => this.inputChangedHandler(event, formElement.id)}
           />
         ))}
-        <Button btnType="Success">ORDER</Button>
+        <Button btnType="Success" disabled={!this.state.formIsValid}>
+          ORDER
+        </Button>
       </form>
     );
 
