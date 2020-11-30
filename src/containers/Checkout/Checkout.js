be7fc817be830092cabac966f749e3f105ebc3d1
Steve <stevestranczek@gmail.com>
2020-11-29 17:02:08 -0700

    Now redirects properly after ordering a burger

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -5,6 +5,7 @@ import { connect } from "react-redux";
 import classes from "./Checkout.module.css";
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
 import ContactData from "../Checkout/ContactData/ContactData";
+import * as actionTypes from "../../store/actions/index";
 
 class Checkout extends Component {
   checkoutCancelledHandler = () => {
@@ -17,9 +18,14 @@ class Checkout extends Component {
 
   render() {
     let summary = <Redirect to="/" />;
+
     if (this.props.ings) {
+      const purchasedRedirect = this.props.purchased ? (
+        <Redirect to="/" />
+      ) : null;
       summary = (
         <div>
+          {purchasedRedirect}
           <CheckoutSummary
             ingredients={this.props.ings}
             checkoutCancelled={this.checkoutCancelledHandler}
@@ -39,6 +45,7 @@ class Checkout extends Component {
 const mapStateToProps = (state) => {
   return {
     ings: state.burgerBuilder.ingredients,
+    purchased: state.order.purchased,
   };
 };
 
