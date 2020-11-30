646db5e598bde7cd61925d97d7ac17bd755aeb98
Steve <stevestranczek@gmail.com>
2020-11-29 17:21:38 -0700

    Added redux to the previous orders page

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -20,7 +20,7 @@ const reducer = (state = initialState, action) => {
         loading: true,
       };
     }
-    case actionTypes.PURCHASE_BURGER_SUCCESS:
+    case actionTypes.PURCHASE_BURGER_SUCCESS: {
       const newOrder = {
         ...action.orderData,
         id: action.orderId,
@@ -31,11 +31,33 @@ const reducer = (state = initialState, action) => {
         purchased: true,
         orders: state.orders.concat(newOrder),
       };
-    case actionTypes.PURCHASE_BURGER_FAIL:
+    }
+    case actionTypes.PURCHASE_BURGER_FAIL: {
       return {
         ...state,
         loading: false,
       };
+    }
+    case actionTypes.FETCH_ORDERS_START: {
+      return {
+        ...state,
+        loading: true,
+      };
+    }
+    case actionTypes.FETCH_ORDERS_SUCCESS: {
+      return {
+        ...state,
+        orders: action.orders,
+        loadng: false,
+      };
+    }
+    case actionTypes.FETCH_ORDERS_FAIL: {
+      return {
+        ...state,
+        loading: false,
+      };
+    }
+
     default:
       return state;
   }
